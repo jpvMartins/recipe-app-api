6c1f8371a53694f67667698e86c9aae243a5d335
João Martins <jpv.martins.20@gmail.com>
2025-06-22 11:34:21 +0100

    Create user and token api

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 
 urlpatterns = [
@@ -28,5 +28,9 @@ urlpatterns = [
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs',
+    ),
+    path(
+        'user/',
+        include('user.urls', namespace='user')
     ),
 ]
